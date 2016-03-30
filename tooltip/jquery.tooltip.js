747a58ee00a5cb3f4be92192e0aa38c4fd81fb46
Ian McBurnie <imcburnie@ebay.com>
2016-03-30 11:51:35 -0700

    Refreshed tooltip with ecommerce-like examples

--- tooltip/jquery.tooltip.js
+++ tooltip/jquery.tooltip.js
@@ -1,6 +1,6 @@
 /**
 * @function jquery.tooltip.js
-* @author Ian McBurnie <imcburnie@ebay.com>
+* @author Ian McBurnie <ianmcburnie@hotmail.com>
 */
 (function ( $ ) {
 
@@ -25,38 +25,36 @@
                 return $overlay.css('top', tooltipWidgetOffset.top - overlayOuterHeight - $window.scrollTop() + 'px');
             };
 
-            $tooltipWidget.not('.tooltip-input').on('mouseenter', function onMouseEnter(e) {
-                $tooltipWidget.trigger('open')
+            $tooltipWidget.not('.tooltip--input').on('mouseenter', function onMouseEnter(e) {
+                $tooltipWidget.trigger('tooltipOpen')
             });
 
-            $tooltipWidget.not('.tooltip-input').on('mouseleave', function onMouseLeave(e) {
-                $tooltipWidget.trigger('dismiss');
+            $tooltipWidget.not('.tooltip--input').on('mouseleave', function onMouseLeave(e) {
+                $tooltipWidget.trigger('tooltipClose');
             });
 
             $tooltipWidget.on('focusin', function onFocusIn(e) {
-                $tooltipWidget.trigger('open');
+                $tooltipWidget.trigger('tooltipOpen');
             });
 
             // Add focusexit plugin
-            $tooltipWidget.focusExit();
-
-            $tooltipWidget.on('focusExit', function onFocusExit(e, data) {
-                $tooltipWidget.trigger('dismiss');
+            $tooltipWidget.focusExit().on('focusExit', function onFocusExit(e, data) {
+                $tooltipWidget.trigger('tooltipClose');
             });
 
-            $tooltipWidget.on('open', function onShow() {
+            $tooltipWidget.on('tooltipOpen', function onShow() {
                 sticky().delay(250).fadeIn(function onFadeIn() {
                     $window.on('scroll', sticky);
                 });
             });
 
-            $tooltipWidget.on('dismiss', function onShow() {
+            $tooltipWidget.on('tooltipClose', function onShow() {
                 $overlay.stop(true).fadeOut(function onFadeOut() {
                     $window.off('scroll');
                 });
             });
 
-            $tooltipWidget.addClass('tooltip-js');
+            $tooltipWidget.addClass('tooltip--js');
 
             setTimeout(function onTimeout() {
                 tooltipWidgetHeight = $tooltipWidget.height();
