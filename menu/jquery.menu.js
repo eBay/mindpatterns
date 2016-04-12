5c21f3e9e0a57a97599ea413d2f742f0a8af360c
Ian McBurnie <imcburnie@ebay.com>
2016-04-12 14:55:44 -0700

    Added sticky flyout

--- menu/jquery.menu.js
+++ menu/jquery.menu.js
@@ -1,39 +1,48 @@
 /**
 * @function jquery.menu.js
 * @version 0.0.1
-* @author Ian McBurnie <imcburnie@ebay.com>
+* @author Ian McBurnie <ianmcburnie@hotmail.com>
 * @requires jquery-button-flyout
 * @requires jquery-common-keydown
 * @requires jquery-roving-tabindex
+* @requires jquery-prevent-document-scroll-keys
 */
 (function ( $ ) {
 
-    var keycodes = {"65":"A","66":"B","68":"D","76":"L","77":"M","78":"N","80":"P"}
+    function createKeyCodeMap() {
+        for (var map = {}, charCode = 65; charCode <= 90; charCode++) {
+            map[charCode] = String.fromCharCode(charCode);
+        }
+
+        return map;
+    }
 
     $.fn.menu = function menu() {
 
         return this.each(function onEach() {
-            var $this = $(this),
-                $button = $this.find('button'),
-                $rootMenu = $this.find('> [role=menu], > *:last-child > [role=menu]').first(),
-                $groups = $rootMenu.find('> div[role=presentation]'),
-                $allmenuitems = $rootMenu.find('> [role^=menuitem], > div > [role^=menuitem], > a'),
-                $links = $rootMenu.find('a'),
-                $buttons = $rootMenu.find('[role=menuitem]'),
-                $checkboxes = $rootMenu.find('[role=menuitemcheckbox]'),
-                $radios = $rootMenu.find('[role=menuitemradio]'),
-                $firstMenuItem = $allmenuitems.first(),
-                $subMenus = $rootMenu.find('[role=menuitem][aria-haspopup=true]'),
-                shortcutKeyMap = {};
+            var $this = $(this);
+            var $button = $this.find('button');
+            var $rootMenu = $this.find('> [role=menu], > *:last-child > [role=menu]').first();
+            var $groups = $rootMenu.find('> div[role=presentation]');
+            var $allmenuitems = $rootMenu.find('> [role^=menuitem], > div > [role^=menuitem], > a');
+            var $links = $rootMenu.find('a');
+            var $buttons = $rootMenu.find('[role=menuitem]');
+            var $checkboxes = $rootMenu.find('[role=menuitemcheckbox]');
+            var $radios = $rootMenu.find('[role=menuitemradio]');
+            var $firstMenuItem = $allmenuitems.first();
+            var $subMenus = $rootMenu.find('[role=menuitem][aria-haspopup=true]');
+            var keyCodeMap = createKeyCodeMap();
+            var shortcutKeyMap = {};
 
+            // store first char of all menu items
             $allmenuitems.each(function(idx) {
-                // store the starting letter of each menu item
                 shortcutKeyMap[$(this).text()[0]] = idx;
             });
 
             // assign id to widget
             $this.nextId('popupmenu');
 
+            // menu is built on top of button-flyout plugin
             $this.buttonFlyout({focusManagement:true});
 
             // listen for specific key presses on all menu items
@@ -45,68 +54,57 @@
             // assign id to menu
             $rootMenu.prop('id', $this.prop('id') + '-menu');
 
+            // create popup menu semantics
             $button
                 .attr('aria-haspopup', 'true')
                 .prop('id', $this.prop('id') + '-button');
 
-            $groups
-                .attr('role', 'presentation');
-
             // all submenus start in collapsed state
             $subMenus.attr('aria-expanded', 'false');
 
-            $allmenuitems.not('a').on('click' , function(e) {
-                $this.trigger('activate', e.target);
+            $allmenuitems.not('a').on('click spaceKeyDown enterKeyDown', function(e) {
+                console.log($(e.target).text());
+                $this.trigger(jQuery.Event( 'menuSelect', {menuValue: 'foo'}));
             });
 
-            $allmenuitems.not('a').on('spaceKeyDown enterKeyDown', function(e) {
-                $this.trigger('activate', e.target);
-            });
-
-            $checkboxes.on('spaceKeyDown enterKeyDown', function(e) {
+            // toggle checkbox state
+            $checkboxes.on('click spaceKeyDown enterKeyDown', function(e) {
                 $(this).attr('aria-checked', $(this).attr('aria-checked') == 'true' ? 'false' : 'true');
             });
 
-            $checkboxes.on('click', function(e) {
-                $(this).attr('aria-checked', $(this).attr('aria-checked') == 'true' ? 'false' : 'true');
-            });
-
-            $radios.on('spaceKeyDown enterKeyDown', function(e) {
+            // toggle radios state
+            $radios.on('click spaceKeyDown enterKeyDown', function(e) {
                 $radios.attr('aria-checked', 'false');
                 $(this).attr('aria-checked', 'true');
             });
 
-            $radios.on('click', function(e) {
-                $radios.attr('aria-checked', 'false')
-                $(this).attr('aria-checked', 'true');
-            });
-
+            // reset all tabindexes when flyout opens and closes
             $this.on('buttonFlyoutOpen buttonFlyoutClose', function onShowOrHide() {
                 $allmenuitems.attr('tabindex', '-1');
             });
 
+            // if char key is pressed, set focus on 1st matching menu item
             $allmenuitems.on('keydown', function(e){
-                var keyPressed = keycodes[e.keyCode];
+                var char = keyCodeMap[e.keyCode];
+                var itemIndex = shortcutKeyMap[char];
 
-                // if a menu item begin with this letter, set focus on it
-                if (keyPressed) {
-                    $allmenuitems.get(shortcutKeyMap[keyPressed]).focus();
+                if (itemIndex) {
+                    $allmenuitems.get(itemIndex).focus();
                 }
-            })
-
-            $this.on('activate', function onActivate(e, item) {
-                alert(item.getAttribute('role') + ': ' + item.innerHTML);
-                $this.trigger('dismiss');
-                setTimeout(function(){
-                    $button.focus();
-                }, 50)
             });
 
-            // call plugin to prevent page scroll
+            // when a menu item selection has been made, set focus back to button
+            $this.on('menuSelect', function onMenuSelect(e) {
+                console.log(e);
+                $button.focus();
+            });
+
+            // use a plugin to prevent page scroll when arrow keys are pressed
             $('[role^=menuitem]').preventDocumentScrollKeys();
 
             // mark widget as js initialised
-            $this.addClass('menu-js');
+            $this.addClass('menu--js');
         });
     };
+
 }( jQuery ));
