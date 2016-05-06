a0b7327817c584674bdfa91deaf1c75ac180cc3a
Ian McBurnie <imcburnie@ebay.com>
2016-05-05 19:23:14 -0700

    Updated bubble help to use button-flyout

--- bubblehelp/jquery.bubblehelp.js
+++ bubblehelp/jquery.bubblehelp.js
@@ -1,100 +1,37 @@
 /**
-* @function jquery.bubblehelp.js
-* @author Ian McBurnie (imcburnie@ebay.com)
+* @file jQuery plugin that creates the basic interactivity for a bubble help flyout
+* @version 0.0.1
+* @author Ian McBurnie <ianmcburnie@hotmail.com>
 */
-(function ( $ ) {
+(function($, window, document, undefined) {
+    $.fn.bubbleHelp = function() {
+        return this.each(function onEach() {
+            var $bubblehelpWidget = $(this);
+            var $button = $bubblehelpWidget.find('> button');
+            var $link = $bubblehelpWidget.find('> a');
 
-    $.fn.bubbleHelp = function () {
+            // if anchor tag, grab referenced content and transform link to button
+            if ($link.length === 1) {
+                var href = $link.attr('href');
+                var contentId = href.substring(href.indexOf('#') + 1);
+                var $content = $('#' + contentId);
 
-        return this.each(function () {
+                $link.attr('aria-label', 'Help');
+                $link.attr('role', 'button');
+                $link.attr('href', ''); // weird bug in chromevox without this
+                $link.text(''); // weird keyboard bug in jaws + ie without this
 
-            var $bubblehelpWidget = $(this),
-                $button = $bubblehelpWidget.find('> a'),
-                href = $button.attr('href'),
-                contentId = href.substring(href.indexOf('#') + 1),
-                $content = $('#' + contentId),
-                hasFocus = false,
-                offset = $bubblehelpWidget.offset(),
-                $liveRegion = $('<span aria-live="polite"/>');
+                $link.after($content);
+            }
 
-            $button.attr('aria-label', 'Help');
-            $button.attr('aria-controls', contentId);
-            $button.attr('aria-expanded', 'false');
-            $button.attr('role', 'button');
+            // call button flyout plugin which does most of the work
+            $bubblehelpWidget.buttonFlyout({isLiveRegion: true});
 
-            // workarounds
-            $button.attr('href', ''); // weird bug in chromevox without this
-            $button.text(''); // weird keyboard bug in jaws + ie without this
+            // button is ready to use
+            $button.prop('disabled', false);
 
-            $content.addClass('bubblehelp-content');
-
-            // plugins
-            $bubblehelpWidget.commonKeyDown();
-            $button.commonKeyDown();
-
-            // place the content inline after the button
-            // this creates a logical reading/tabindex order
-            $liveRegion.append($content);
-            $button.after($liveRegion);
-
-            $content.css('left', offset.left + $button.outerWidth());
-
-            $button.on('click', function (e) {
-                e.preventDefault();
-                $(this).focus();
-                $bubblehelpWidget.trigger('toggle');
-            });
-
-            $button.on('spaceKeyDown', function (e) {
-                e.preventDefault();
-                $bubblehelpWidget.trigger('toggle');
-            });
-
-            $content.on('focusin', function (e) {
-                hasFocus = true;
-            });
-
-            $content.on('focusout', function (e) {
-                hasFocus = false;
-            });
-
-            $bubblehelpWidget.on('toggle', function (e) {
-                var eventName = ($button.attr('aria-expanded') === 'false') ? 'show' : 'hide';
-
-                $bubblehelpWidget.trigger(eventName);
-            });
-
-            $bubblehelpWidget.on('show', function (e) {
-                $button.attr('aria-expanded', 'true');
-                $content.fadeIn('fast');
-
-                $bubblehelpWidget.trigger('sticky');
-
-                $(window).on('scroll', function(e) {
-                    $bubblehelpWidget.trigger('sticky');
-                });
-            });
-
-            $bubblehelpWidget.on('hide', function (e) {
-                $(window).off('scroll');
-
-                $button.attr('aria-expanded', 'false');
-                $content.fadeOut('fast');
-
-                if (hasFocus === true) {
-                    $button.focus();
-                }
-            });
-
-            $bubblehelpWidget.on('escapeKeyDown', function (e) {
-                $bubblehelpWidget.trigger('hide');
-            });
-
-            $bubblehelpWidget.on('sticky', function (e) {
-                $content.css('top', offset.top - $(window).scrollTop() - Math.round($content.outerHeight() / 3) + 'px');
-            });
-
-            $bubblehelpWidget.addClass('bubblehelp-js');
+            // mark the widget as done
+            $bubblehelpWidget.addClass('bubblehelp--js');
         });
     };
-}( jQuery ));
+}(jQuery, window, document));
