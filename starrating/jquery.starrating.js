a6c23c264704ec4bfd36a01771fab4054de55952
Ian McBurnie <imcburnie@ebay.com>
2016-08-18 10:54:41 -0700

    Removed development mode from star rating

--- starrating/jquery.starrating.js
+++ starrating/jquery.starrating.js
@@ -7,7 +7,19 @@
             var $stars = $widget.find('input');
             var isChecked = $widget.find('input:checked').length > 0;
 
+            $widget.one('click', function() {
+                $(this).removeClass('starrating--unselected');
+            });
+
+            if (isChecked === false) {
+                $widget.addClass('starrating--unselected');
+            }
+
             /*
+            // the following code would be needed if labels and inputs are not all siblings in the DOM
+            // in that case, CSS adjacent sibling selectors would not work
+            // example of this to come
+
             var includedStars = function(index) {
                 return index < $stars.index($widget.find('input:checked'));
             };
@@ -20,14 +32,6 @@
 
             update();
             */
-
-            $widget.one('click', function() {
-                $(this).removeClass('starrating--unselected');
-            });
-
-            if (isChecked === false) {
-                $widget.addClass('starrating--unselected');
-            }
         });
     };
 }( jQuery ));
