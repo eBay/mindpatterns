b8e3a510ca96250fb7e69048976346d55c1169de
Ian McBurnie <imcburnie@ebay.com>
2016-08-24 14:17:32 -0700

    Added pure ARIA radios example with no fallbacks
    This example requires JS and stores no form data. Useful for ‘dumb’
    radio buttons.

---
+++ radio/jquery.ariaradiogroup.js
@@ -0,0 +1,65 @@
+/**
+* @function jquery.ariaradiogroup.js
+* @desc Please DO NOT copy this code to production! This is 'quick & ugly, just make it work!' code.
+* @author Ian McBurnie <ianmcburnie@hotmail.com>
+* @desc Sample implementation for a pure aria radio group with no form fallbacks.
+* @requires jquery-next-id
+* @requires jquery-common-keys
+* @requires jquery-roving-tabindex
+* @requires jquery-prevent-scroll-keys
+*/
+(function ( $ ) {
+
+    $.fn.ariaRadioGroup = function ariaRadioGroup() {
+
+        return this.each(function onEachAriaRadioGroup() {
+
+            var $widget = $(this),
+                $fieldset = $widget.find('.aria-radio-group__fieldset'),
+                $labels = $fieldset.find('.aria-radio__label'),
+                hasCheckedRadio = $fieldset.find('[aria-checked=true]').length > 0,
+                checkedRadioIndex = hasCheckedRadio ? $widget.find('[aria-checked=true]').parent().index() : 0;
+
+            // all common key events on radio buttons are delegated to widget
+            $widget.commonKeyDown('[role=radio]');
+
+            // spacebar triggers 'check' event
+            $widget.on('spaceKeyDown', function onSpaceKeyDown(e) {
+                $widget.trigger('check', e.originalEvent.target);
+            });
+
+            // enter key triggers 'ariaRadioGroupSelect' event with checked radio index and label text
+            $widget.on('enterKeyDown', function onEnterKeyDown(e) {
+                var $checkedRadio = $fieldset.find('[aria-checked=true]');
+                $widget.trigger('ariaRadioGroupSelect', { index: $checkedRadio.parent().index(), label: $checkedRadio.next().text() });
+            });
+
+            // clicking a label checks the associated checkbox
+            $labels.on('click', function(e) {
+                $widget.trigger('check', $(e.target).prev());
+            });
+
+            // create a roving tab index on custom radios
+            $widget.rovingTabindex('[role=radio]', {activeIndex: checkedRadioIndex});
+
+            // update radiogroup state on check event or rovingTabindex change
+            $widget.on('check rovingTabindexChange', function onCheckAndRove(e, radioButton) {                
+                var $radioButton = $(radioButton),
+                    $checkedRadio = $fieldset.find('[aria-checked=true]');
+
+                if ($radioButton[0] !== $checkedRadio[0]) {
+                    // uncheck currently checked radio
+                    $checkedRadio.attr('aria-checked', 'false');
+                    // check the radio
+                    $radioButton.attr('aria-checked', 'true');
+                }
+            });
+
+            // call plugin to prevent page scroll
+            $widget.preventScrollKeys('[role=radio]');
+
+            // mark our widget as intialised
+            $widget.addClass('aria-radio-group--js');
+        });
+    };
+}( jQuery ));
