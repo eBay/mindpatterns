a225c8c5a77ca64f580fb0a2825c3e02b73e46a2
Ian McBurnie <ianmcburnie@hotmail.com>
2017-02-27 20:39:02 -0800

    Update form validation example

--- _messaging/formvalidation/jquery.formvalidation.js
+++ _messaging/formvalidation/jquery.formvalidation.js
@@ -5,21 +5,22 @@
 */
 (function ( $ ) {
 
-    $.fn.formvalidation = function formValidation() {
+    $.fn.formValidation = function formValidation() {
 
-        // this plugin is intentionally not reusable!
-        // the validation routine and error messages are hardcoded.
+        // this plugin is intentionally not reusable for now!
+        // the validation routine and error messages are hardcoded for demo purposes only!
 
         var $form = $(this),
             $regionnotice = $('<div class="regionnotice" tabindex="-1">'),
             $errorHeading = $('<h3>Error! The form could not be submitted due to invalid entries.</h3><p>Please fix the following fields:</p>'),
             $listOfErrors = $('<ol>'),
             $elementnotice1 = $('<p class="elementnotice" id="age_error">Please enter a valid age (for example, 35)</p>'),
-            $elementnotice2 = $('<p class="elementnotice" id="shoesize_error">Please enter a valid shoe size (for example, 8.5)</p>'),
-            $firstError = $('<li><a href="#age_container">Age - please enter a valid age (for example, 35)</a></li>'),
-            $secondError = $('<li><a href="#shoesize_container">Shoe-size - please enter a valid shoe size (for example, 8.5)</a></li>');
-            $ageInput = $('#age');
-            $shoeSizeInput = $('#shoesize');
+            $elementnotice2 = $('<p class="elementnotice" id="shoesize_error">Please enter a valid shoe size (for example, 8.5)</p>');
+
+        var errorLinks = [
+            '<a href="#age">Age - please enter a valid age (for example, 35)</a>',
+            '<a href="#shoesize">Shoe-size - please enter a valid shoe size (for example, 8.5)</a>'
+        ];
 
         // prevent built-in validation (it conflicts with our custom validation)
         $form.attr('novalidate', 'novalidate');
@@ -27,19 +28,34 @@
         $regionnotice.append($errorHeading);
         $regionnotice.append($listOfErrors);
 
+        // insert hardcoded errors
+        errorLinks.forEach(function(el) {
+            $listOfErrors.append('<li>' + el + '</li>');
+        });
+
+        $listOfErrors.on('click', 'a', function(e) {
+            e.preventDefault();
+            var href = this.href;
+            var targetId = href.substring(href.indexOf('#'));
+            $(targetId).focus();
+        });
+
+        $form.one('submit', function onSubmit(e) {
+            // setup hardcoded input notices
+            $form.find('input[type=text]').attr('aria-invalid', 'true');
+
+            $('#age_field').append($elementnotice1);
+            $('#shoesize_field').append($elementnotice2);
+
+            $('#age').attr('aria-describedby', 'age_error');
+            $('#shoesize').attr('aria-describedby', 'shoesize_error');
+
+            $form.prepend($regionnotice);
+        });
+
         $form.on('submit', function onSubmit(e) {
             e.preventDefault();
-
-            // insert hardcoded errors
-            $listOfErrors.append($firstError).append($secondError);
-            $form.find('input[type=text]').attr('aria-invalid', 'true');
-            $('#age_container').append($elementnotice1);
-            $('#shoesize_container').append($elementnotice2);
-            $form.prepend($regionnotice);
-            $ageInput.attr('aria-describedby', 'age_error');
-            $shoeSizeInput.attr('aria-describedby', 'shoesize_error');
-
             $regionnotice.focus();
         });
     };
-}( jQuery ));
+}(jQuery));
