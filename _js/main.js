1d145de20381585d81333555f0bf9e832cab4223
Ian McBurnie <ianmcburnie@users.noreply.github.com>
2020-03-24 16:15:49 -0700

    Added switch example

--- _js/main.js
+++ _js/main.js
@@ -39,6 +39,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
     const MenuButton = require('./menu-button.js');
     const Pagination = require('./pagination.js');
     const StarRating = require('./star-rating.js');
+    const Switch = require('./switch.js');
     const Tabs = require('./tabs.js');
     const Tile = require('./tile.js');
     const Tooltip = require('./tooltip.js');
@@ -233,6 +234,19 @@ document.addEventListener("DOMContentLoaded", function(e) {
 
     document.querySelectorAll('.star-rating').forEach(function(widgetEl) {
         pageWidgets.push(new StarRating(widgetEl));
+    });
+
+    document.querySelectorAll('.switch').forEach(function(widgetEl) {
+        pageWidgets.push(new Switch(widgetEl));
+
+        widgetEl.addEventListener('switch-toggle', function(e) {
+            console.log(e.type, e.detail);
+            if (e.detail.value === 'on') {
+                document.body.classList.add('lights-on');
+            } else {
+                document.body.classList.remove('lights-on');
+            }
+        });
     });
 
     document.querySelectorAll('.tabs').forEach(function(widgetEl) {
